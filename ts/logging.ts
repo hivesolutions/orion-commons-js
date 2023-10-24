983a1c3b7a39aa851a944de10ddf448a15583469
João Magalhães <joamag@gmail.com>
2023-10-24 10:35:49 +0100

    chore: initial project structure

---
+++ ts/logging.ts
@@ -0,0 +1,20 @@
+import util from "hive-js-util";
+
+const { Logging } = util;
+
+const setupLogging = async () => {
+    const level = ((await confP("LEVEL", "DEBUG")) as string).toUpperCase();
+
+    const logger = Logging.getLogger(undefined, {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        level: (<any>Logging.constants)[level]
+    });
+
+    if (Logging.ConsolaHandler.isReady()) {
+        logger.addHandler(new Logging.ConsolaHandler());
+        logger.setFormatter(new Logging.SimpleFormatter("{asctime} {message}"));
+    } else {
+        logger.addHandler(new Logging.StreamHandler());
+        logger.setFormatter(new Logging.SimpleFormatter());
+    }
+};
