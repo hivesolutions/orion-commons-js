66771f384685db6e1cbd040b12d3709a37c17850
João Magalhães <joamag@gmail.com>
2023-10-24 13:04:31 +0100

    fix: unit test running

---
+++ test/example.test.ts
@@ -0,0 +1,10 @@
+import { assert } from "chai";
+
+describe("Example", function () {
+    describe("#example()", function () {
+        it("should be able to run simple tests", () => {
+            assert.strictEqual(1, 1);
+            assert.strictEqual("hello", "hello");
+        });
+    });
+});
